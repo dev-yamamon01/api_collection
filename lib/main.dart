8c6ede28254a5d4117bec7f99dfdf75de129c765
dev-yamamon <dev.yamamon01@gmail.com>
2024-12-17 00:10:50 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Udemy outputs',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'API Collection'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Text('This is My App')
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.post_add_outlined), label: '郵便番号'),
+        BottomNavigationBarItem(icon: Icon(Icons.sunny), label: '天気'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.circle), label: 'ポケモン'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント'),
+      ],
+      type: BottomNavigationBarType.fixed,
+    ));
+  }
+}
